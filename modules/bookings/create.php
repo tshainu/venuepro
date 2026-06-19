e6f18df9b3ac290fb2f7f7ee68982c782b46d043
VenuePro <venuepro@deploy.com>
2026-06-19 06:43:35 +0000

    fix: undefined key warning on guest_count in bookings/create.php

--- modules/bookings/create.php
+++ modules/bookings/create.php
@@ -488,7 +488,7 @@ $evtType = $_POST['event_type'] ?? ($fromInquiry['event_type'] ?? '');
             <label class="form-label">Guest Count</label>
             <div class="input-icon-wrap">
               <svg class="input-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"/><circle cx="9" cy="7" r="4"/><path d="M23 21v-2a4 4 0 00-3-3.87M16 3.13a4 4 0 010 7.75"/></svg>
-              <input type="number" name="guest_count" class="form-control" min="0" value="<?= intval($_POST['guest_count'] ?: ($fromInquiry['guest_count'] ?? '')) ?>" placeholder="e.g. 200">
+              <input type="number" name="guest_count" class="form-control" min="0" value="<?= intval(($_POST['guest_count'] ?? '') ?: ($fromInquiry['guest_count'] ?? '')) ?>" placeholder="e.g. 200">
             </div>
           </div>
         </div>
