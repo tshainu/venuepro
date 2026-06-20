e1552c86ffc91d830172dffb2d1b640ac0f47783
VenuePro SA <superadmin@venuepro.lk>
2026-06-20 07:43:36 +0000

    fix: supervisord path in entrypoint (was pointing to wrong conf)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,4 +6,4 @@ echo "Starting on port $APP_PORT"
 # Replace port placeholder in nginx config
 sed -i "s/PORT_PLACEHOLDER/$APP_PORT/g" /etc/nginx/nginx.conf
 
-exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/services.conf
